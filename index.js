7508faebe183e0469497bffbec66e1416749cbc4
elo-erin <erin.pericas@gmail.com>
2017-12-24 12:59:08 +0100

    modif des pages googlemap REUSSI merci ruben !

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ function toggleNavMenu() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+// Create all Google maps and add associated markers
+function initMaps()
+{
+	// Define coordinates
+	centerMont = new google.maps.LatLng(48.636017,-1.511114);
+	centerJapan = new google.maps.LatLng(37.902552,139.023095);
+
+	// Define map options
+	var optionsMont = {
+		center: centerMont,
+		zoom:12, scrollwheel: false, draggable: false,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var optionsJapan = {
+		center: centerJapan,
+		zoom:12, scrollwheel: false, draggable: false,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+
+	// Create maps
+	var mapMont = new google.maps.Map(document.getElementById("mapMont"), optionsMont);
+	var mapJapan = new google.maps.Map(document.getElementById("mapJapan"), optionsJapan);
+
+	// Add markers
+	var markerMont = new google.maps.Marker({
+		position: centerMont,
+	});
+	markerMont.setMap(mapMont);
+	var markerJapan = new google.maps.Marker({
+		position: centerJapan,
+	});
+	markerJapan.setMap(mapJapan);
+}
